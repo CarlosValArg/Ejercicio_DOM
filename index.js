a1f3de00880da0847870d196e70e8270f33de942
Carlos Valencia <cavalar1999@gmail.com>
2024-09-05 00:15:40 -0600

    Se agrega el ejercicio de storage terminado

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const btnGuardar = document.getElementById("btnGuardar");
-const btnClear = document.getElementById("btnClear");
-const txtNombre = document.getElementById("Nombre");
+const txtNombre = document.getElementById("txtNombre");
+const alertValidaciones = document.getElementById("alertValidaciones");
 
-let isValid = true
-
+btnGuardar.addEventListener("click", function(event){
+    event.preventDefault();
+        alertValidaciones.innerHTML="";
+        alertValidaciones.style.display="none";
+    if(txtNombre.value.length<3){
+        alertValidaciones.innerHTML="Nombre incorrecto";
+        alertValidaciones.style.display="block";
+    }/**if */
+    localStorage.setItem("nombre", txtNombre.value);
+});
--- welcome.js
+++ welcome.js
@@ -1,2 +1,16 @@
-const btnGuardar = document.getElementById("alertWarning");
-const btnElim =document.getElementById("btnElim")
+const alertMensaje = document.getElementById("alertMensaje");
+const btnElim =document.getElementById("btnElim");
+
+window.addEventListener("load",function(event){
+
+    if(this.localStorage.getItem("nombre") !=null){
+        alertMensaje.innerHTML =`Hello, ${this.localStorage.getItem("nombre")}, Welcome again`;
+    }/**if nombre!=null */
+    alertMensaje.style.display="block";
+}); /**load */
+
+btnElim.addEventListener("click",function(event){
+    event.preventDefault();
+    localStorage.removeItem("nombre");
+    location.href="welcome.html"
+})
